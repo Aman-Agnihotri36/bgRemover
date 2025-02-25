29c4c4126c18bbcb5188449ca45b59d7900c0c99
Aman-Agnihotri36 <agnihotriaman36@gmail.com>
2025-02-25 21:17:44 +0530

    done

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -101,7 +101,8 @@ function HeroSection() {
         <div className='flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-4 mt-10 lg:px-44 sm:mt-20'>
             <div >
                 <h1 className='text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-700 leading-tight'>Remove the <br className='max-md:hidden' /> <span className='sm:bg-gradient-to-r text-pink-500 sm:from-violet-600 sm:to-fuchsia-500 sm:bg-clip-text sm:text-transparent'>background</span> from <br className='max-md:hidden' /> images for free.</h1>
-                <p className='my-6 text-[15px] text-gray-500'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus qui veniam voluptatum <br /> quod consequuntur error, facilis dolorem deleniti possimus reprehenderit ipsum.</p>
+                <p className='my-6 text-[15px] text-gray-500'>
+                    Effortlessly remove backgrounds from images with our BG Remover tool. Simply upload your image, and it will instantly process  <br />to give you a clean, transparent background. Ideal for presentations, e-commerce, marketing materials, and more! </p>
 
                 <div >
                     {isSignedIn ? (<div >  <Label htmlFor="picture" className='inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700'>
